b56fabd1c6458c574f72ff643e282de777c6f719
Danzakuduroch <rassistavanturist7@gmail.com>
2025-06-24 20:52:40 +0500

    добавлена секция о комиссариате

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Военный комиссариат</title>
-	@vite('resources/css/app.css')
+    @vite('resources/css/app.css')
 </head>
+
 <body class="page">
     <header class="header">
         <div class="container">
@@ -15,15 +17,31 @@
                 </div>
                 <nav class="header__nav nav">
                     <ul class="nav__list">
-                        <li class="nav__item"><a href="#">Главная</a></li>
-                        <li class="nav__item"><a href="#">Услуги</a></li>
-                        <li class="nav__item"><a href="#">Новости</a></li>
-                        <li class="nav__item"><a href="#">Контакты</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Главная</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Услуги</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Новости</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Контакты</a></li>
                     </ul>
                 </nav>
             </div>
         </div>
     </header>
-
+    <main class="main">
+        <div class="container">
+            <section class="intro">
+                <h2 class="intro__title">О военном комиссариате</h2>
+                <p class="intro__text">
+                    Военный комиссариат — это государственное учреждение, осуществляющее организацию воинского учёта, призыв граждан на военную службу, оформление документов, а также предоставление консультаций по вопросам, связанным с воинской обязанностью и службой.
+                </p>
+                <ul class="intro__list">
+                    <li class="intro__item">Проведение призыва на военную службу</li>
+                    <li class="intro__item">Ведение воинского учёта граждан</li>
+                    <li class="intro__item">Оформление справок и документов</li>
+                    <li class="intro__item">Консультации по вопросам воинской обязанности</li>
+                </ul>
+            </section>
+        </div>
+    </main>
 </body>
+
 </html>
