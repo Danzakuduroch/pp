5d2fc035482c4aa61cb452123acd862a10402bae
Danzakuduroch <rassistavanturist7@gmail.com>
2025-06-24 20:45:34 +0500

    добавлена шапка

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Военный комиссариат</title>
+	@vite('resources/css/app.css')
+</head>
+<body class="page">
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <div class="header__brand">
+                    <h1 class="header__title">Военный комиссариат</h1>
+                </div>
+                <nav class="header__nav nav">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#">Главная</a></li>
+                        <li class="nav__item"><a href="#">Услуги</a></li>
+                        <li class="nav__item"><a href="#">Новости</a></li>
+                        <li class="nav__item"><a href="#">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
