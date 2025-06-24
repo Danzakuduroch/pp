54a84b4dd2448359d080ef26e5f6876460278329
Danzakuduroch <rassistavanturist7@gmail.com>
2025-06-24 20:33:56 +0500

    добавлен контроллер и роут

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommissariatController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CommissariatController::class, 'index']);
