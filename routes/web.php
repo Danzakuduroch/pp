d9f016aa30e4b41e086dc516fbdf47d06835cd4f
Danzakuduroch <rassistavanturist7@gmail.com>
2025-06-25 09:48:52 +0500

    добавлено роут и контроллер для страницы призывников

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommissariatController;
 use App\Http\Controllers\ContractController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ConscriptController;
 
 Route::get('/', [CommissariatController::class, 'index'])->name('index');
 Route::get('/contracts', [ContractController::class, 'index'])->name('contracts.index');
 Route::get('/form', [ContractController::class, 'form'])->name('form.index');
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/conscripts', [ConscriptController::class, 'index'])->name('conscripts.index');
